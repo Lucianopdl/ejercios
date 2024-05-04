ed36a80c1af92ae94da97bdeec9eb5069e5980d4
Lucianopdl <135555802+Lucianopdl@users.noreply.github.com>
2024-05-04 18:53:18 -0300

    mejoras en el js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const txtOp1= document.getElementById("op1");
-const txtOp2= document.getElementById("op2");
-const parametro= document.getElementById("parametro");
-const btnCalcular= document.getElementById("calcular")
-const pResultado= document.getElementById("resultado");
+const txtOp1 = document.getElementById("op1");
+const txtOp2 = document.getElementById("op2");
+const parametro = document.getElementById("parametro");
+const btnCalcular = document.getElementById("calcular");
+const pResultado = document.getElementById("resultado");
 
+btnCalcular.addEventListener('click', ejercicioUno);
 
-btnCalcular.addEventListener('click', calcular)
 function ejercicioUno() {
-    const parametro = document.getElementById("parametro").value;
-    const txtOp1 = parseFloat(document.getElementById("txtOp1").value);
-    const txtOp2 = parseFloat(document.getElementById("txtOp2").value);
+    const operador1 = parseFloat(txtOp1.value);
+    const operador2 = parseFloat(txtOp2.value);
+    const operacion = parametro.value;
 
-    if (parametro === "+" || parametro === "-" || parametro === "/" || parametro === "*") {
-        let resultado;
-        switch (parametro) {
-            case "+":
-                resultado = txtOp1 + txtOp2;
-                break;
-            case "-":
-                resultado = txtOp1 - txtOp2;
-                break;
-            case "/":
-                resultado = txtOp1 / txtOp2;
-                break;
-            case "*":
-                resultado = txtOp1 * txtOp2;
-                break;
-        }
-        document.getElementById("pResultado").innerText = "=" + resultado;
-    } else {
-        document.getElementById("pResultado").innerText = "cálculo imposible";
+    let resultado;
+    switch (operacion) {
+        case "+":
+            resultado = operador1 + operador2;
+            break;
+        case "-":
+            resultado = operador1 - operador2;
+            break;
+        case "/":
+            resultado = operador1 / operador2;
+            break;
+        case "*":
+            resultado = operador1 * operador2;
+            break;
+        default:
+            resultado = "cálculo imposible";
     }
+
+    pResultado.innerText = "=" + resultado;
 }
+
 
 
 
